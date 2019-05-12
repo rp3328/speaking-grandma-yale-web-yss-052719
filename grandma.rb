99576c969f57bb68ca09d1040c08ad402fbab64d
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-12 10:15:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(var)
   if var == var.upcase
     return "NO, NOT SINCE 1938!"
   elsif var == 'I LOVE YOU GRANDMA!'
-    return "I LOVE YOU TOO PUMPKIN!"
+    return 'I LOVE YOU TOO PUMPKIN!'
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
