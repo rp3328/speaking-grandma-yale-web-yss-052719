1a8784a0060d2575a0edaf74393deaf2a32eb4c8
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-12 10:01:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,8 @@
 def speak_to_grandma(string1)
   if "string1" == "string1.uppercase"
     puts "NO, NOT SINCE 1938!"
-  elsif 'string1' == 'I LOVE YOU GRANDMA!'
+  elsif "string1" == "I LOVE YOU GRANDMA!"
     puts 'I LOVE YOU TOO PUMPKIN!'
-  else puts 'HUH?! SPEAK UP, SONNY!'
+  else 
+    puts 'HUH?! SPEAK UP, SONNY!'
 end
